63c073163af9da244d3af7081dee770654f127cb
mkgugan22 <mkgugan22@gmail.com>
2026-04-09 11:19:57 +0530

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import { Provider } from "react-redux";
 import store from "./store";
 import App from "./App";
 import "./styles/global.css";
+import { ThemeProvider } from "./context/ThemeContext";
+import { AuthProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
